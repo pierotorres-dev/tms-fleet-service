69e4c894e104e73a3580515f26a47966cdb8fdf1
Piero Torres <torresartica1701@gmail.com>
2025-05-05 07:52:29 -0500

    first commit

---
+++ src/main/java/com/dliriotech/tms/fleetservice/TmsFleetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.fleetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsFleetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsFleetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dliriotech/tms/fleetservice/TmsFleetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.fleetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsFleetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
