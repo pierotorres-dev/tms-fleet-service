d15bde90c621535d4fc075d8929c8ab0a8a7be5c
Piero Torres <torresartica1701@gmail.com>
2025-05-23 11:30:23 -0500

    fix: remove cors config

--- src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
+++ src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
@@ -8,13 +8,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
