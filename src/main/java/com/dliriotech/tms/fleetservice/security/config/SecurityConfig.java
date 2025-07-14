b5bae35983591d43e0bcd22770b7c1ecf23acc5c
pierotorres-dev <torresartica1701@gmail.com>
2025-07-14 18:18:58 -0500

    refactor(security): remove security filter chain

--- src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
+++ src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
@@ -1,21 +1,14 @@
 package com.dliriotech.tms.fleetservice.security.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final ServiceAuthFilter serviceAuthFilter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -23,23 +16,9 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**", "/openapi.yaml").permitAll()
-                        .pathMatchers("/api/v1/equipos/**").permitAll()
-                        .pathMatchers("/api/v1/observaciones-equipo/**").permitAll()
-                        .pathMatchers("/api/v1/catalogos/**").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .addFilterBefore(serviceAuthFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-                .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec
-                        .authenticationEntryPoint((exchange, ex) ->
-                                Mono.fromRunnable(() ->
-                                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED)
-                                )
-                        )
-                        .accessDeniedHandler((exchange, denied) ->
-                                Mono.fromRunnable(() ->
-                                        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN)
-                                )
-                        )
+                        .pathMatchers("/api/v1/**").permitAll()
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().permitAll()
                 )
                 .build();
     }
