6e87e6b7afa889792b903e844d38fa3eeeda41df
pierotorres-dev <torresartica1701@gmail.com>
2025-07-16 19:08:30 -0500

    refactor: remove redundant filter chain

--- src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
+++ src/main/java/com/dliriotech/tms/fleetservice/security/config/SecurityConfig.java
@@ -1,16 +1,16 @@
 package com.dliriotech.tms.fleetservice.security.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebFluxSecurity
+//@Configuration
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
+    /*@Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
@@ -21,5 +21,5 @@ public class SecurityConfig {
                         .anyExchange().permitAll()
                 )
                 .build();
-    }
+    }*/
 }
